5ce801b611832dba3b67c3eaebff06f9cc4ff0d6
Camiel <camielk@live.nl>
2015-06-25 09:53:49 +0200

    database

--- LamaGaan/CC/CCVrijwilligersBeheren.cs
+++ LamaGaan/CC/CCVrijwilligersBeheren.cs
@@ -9,42 +9,19 @@ namespace LamaGaan.CC
 {
     public class CCVrijwilligersBeheren
     {
-        private Persoon nieuwePersoon = new Persoon();
-
-
+        private Persoon persoon = new Persoon();
 
         //geeft een list met strings van alle namen van de personen in Persoon
         public List<string> GetAllPersoonNamen()
         {
-            List<string> namen = new Persoon().GetAllPersoonNamen();
+            List<string> namen = persoon.GetAllPersoonNamen();
             return namen;
         }
-
 
         //functie geeft een dictionary met daarin alle attributen van de gegeven persoon
         public Dictionary<string, object> GetPersoonInfo(string naam)
         {
-            Dictionary<string, object> persoonDict = new Dictionary<string, object>();
-
-            using (LamaGaanModelContainer context = new LamaGaanModelContainer())
-            {
-                IQueryable<Persoon> naamQuery =
-                    from x in context.Persoon
-                    where x.Naam == naam
-                    select x;
-
-                foreach (Persoon persoon in naamQuery)
-                {
-                    if (persoon != null)
-                    {
-                        persoonDict.Add("Naam", persoon.Naam.ToString());
-                        persoonDict.Add("Email", persoon.Email.ToString());
-                        persoonDict.Add("Geboorte Datum", persoon.GeboorteDatum.ToString("MM-dd-yyyy"));
-                        persoonDict.Add("Start Datum", persoon.Startdatum.ToString("MM-dd-yyyy"));
-                    }
-                }
-            }
-
+            Dictionary<string, object> persoonDict = persoon.GetPersoonInfo(naam);
             return persoonDict;
         }
     }
