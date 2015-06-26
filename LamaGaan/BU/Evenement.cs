c4cba50faf11a37b8772b0903e7fa44d740919a9
Grisco <Dennis.kooy@live.nl>
2015-06-26 14:11:06 +0200

    Usecase start

--- LamaGaan/BU/Evenement.cs
+++ LamaGaan/BU/Evenement.cs
@@ -14,9 +14,9 @@ namespace LamaGaan.BU
 
             using (LamaGaanModelContainer context = new LamaGaanModelContainer())
             {
-                IQueryable<Evenement> chooseEvenements 
-                    = from x in context.Evenement 
-                      where x.Datum.ToString("MM-dd-yyyy") == date 
+                IQueryable<Evenement> chooseEvenements
+                    = from x in context.Evenement
+                      where x.Datum.ToString("MM-dd-yyyy") == date
                       select x;
                 foreach (Evenement evenement in chooseEvenements)
                 {
@@ -28,24 +28,41 @@ namespace LamaGaan.BU
                         eventDictionary.Add("AantalPersonen", AantalPersonen);
                         eventDictionary.Add("Korting", Korting);
                     }
-                   
+
                 }
             }
             return eventDictionary;
         }
 
-        public Dictionary<string,object>[] GetAllEvents()
+        public List<string> GetAllEvents()
         {
-            Dictionary<string, object>[] allevents = new Dictionary<string, object>[];
+            List<string> events = new List<string>();
             using (LamaGaanModelContainer context = new LamaGaanModelContainer())
             {
-                IQueryable<Evenement> allEvenements = from x in context.Evenement select x;
-                foreach (Evenement evenement in allEvenements)
+                IQueryable<Evenement> eventQuery = from a in context.Evenement select a;
+                foreach (Evenement a in eventQuery)
                 {
-
-                    
+                    events.Add(a.Naam.ToString());
                 }
             }
+            return events;
         }
+        //public Dictionary<string,object> GetAllEvents()
+        //{
+        //    Dictionary<string, object> allevents = new Dictionary<string, object>();
+        //    using (LamaGaanModelContainer context = new LamaGaanModelContainer())
+        //    {
+        //        IQueryable<Evenement> allEvenementsQ = from x in context.Evenement select x;
+        //        foreach (Evenement evenement in allEvenementsQ)
+        //        {
+        //            allevents.Add("Naam", Naam);
+        //            allevents.Add("Soort", Soort);
+        //            allevents.Add("Datum", Datum);
+        //            allevents.Add("AantalPersonen", AantalPersonen);
+        //            allevents.Add("Korting", Korting);
+        //            allevents.Add("RowEnd", "RowEnd");
+        //        }
+        //        return allevents;
+        //    }
     }
 }
---
+++ LamaGaan/User Interface/Evenementen.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using LamaGaan.BU;
+
+namespace LamaGaan.User_Interface
+{
+    public partial class Evenementen : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Evenement allEvenement = new Evenement();
+            List<string> namenList = allEvenement.GetAllEvents();
+            foreach(string naam in namenList)
+            {
+                Label1.Text += naam + "<br />";
+            }
+        }
+    }
+}
