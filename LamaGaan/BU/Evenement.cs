6314f2be55e0cc3bb137e2b87fb9b280d6878fc4
Grisco <Dennis.kooy@live.nl>
2015-06-26 13:15:19 +0200

    Evenement
    Added event

--- LamaGaan/BU/Evenement.cs
+++ LamaGaan/BU/Evenement.cs
@@ -1,15 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Web;
 
 namespace LamaGaan.BU
 {
     partial class Evenement
     {
-        //public void test()
-        //{
-            
-        //}
+        public Dictionary<string, object> GetDateEvents(string date)
+        {
+            Dictionary<string, object> eventDictionary = new Dictionary<string, object>();
+
+            using (LamaGaanModelContainer context = new LamaGaanModelContainer())
+            {
+                IQueryable<Evenement> chooseEvenements 
+                    = from x in context.Evenement 
+                      where x.Datum.ToString("MM-dd-yyyy") == date 
+                      select x;
+                foreach (Evenement evenement in chooseEvenements)
+                {
+                    if (evenement != null)
+                    {
+                        eventDictionary.Add("Naam", Naam);
+                        eventDictionary.Add("Soort", Soort);
+                        eventDictionary.Add("Datum", Datum);
+                        eventDictionary.Add("AantalPersonen", AantalPersonen);
+                        eventDictionary.Add("Korting", Korting);
+                    }
+                   
+                }
+            }
+            return eventDictionary;
+        }
+
+        public Dictionary<string,object>[] GetAllEvents()
+        {
+            Dictionary<string, object>[] allevents = new Dictionary<string, object>[];
+            using (LamaGaanModelContainer context = new LamaGaanModelContainer())
+            {
+                IQueryable<Evenement> allEvenements = from x in context.Evenement select x;
+                foreach (Evenement evenement in allEvenements)
+                {
+
+                    
+                }
+            }
+        }
     }
 }
